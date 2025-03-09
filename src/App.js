a852a8d375138c19920ab01285b81757a18bd044
RithuAj <rithuaj@gmail.com>
2025-03-10 01:36:32 +0530

    mode toggle  and alert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 
 import './App.css';
+import AboutUs from './components/AboutUs';
+import Alert from './components/Alert';
 import Navbar from './components/navbar';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react';
 
 function App() {
+
+  const [mode,setMode] =useState('light');
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor="grey";  
+      showAlert("dark mode enabled","success");
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor="white"; 
+      showAlert("Light mode enabled","success");
+    }
+  }
+
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+
+    })
+    setTimeout(() => {
+      setAlert(null);      
+    }, 1500);
+    
+  }
+
   return (
     <>
-    <Navbar></Navbar>
+    <Navbar mode={mode} toggleMode={toggleMode} ></Navbar>
+    <Alert alert={alert}></Alert>
     <div className="container my-3">
-      <TextForm defaultContent={"Play with the Text Here :)"}></TextForm>
+      <TextForm defaultContent={"Play with the Text Here :)"} mode={mode} showAlert={showAlert}></TextForm>
+      {/* <AboutUs></AboutUs> */}
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,17 +1,27 @@
 import React,{useState} from 'react'
 
 export default function TextForm(props) {
+   
  const[text,setText]=useState("Enter text from here")
 
  const handleUpButton=()=>{
     // console.log("Upper case button clicked");
     let newText=text.toUpperCase();
     setText(newText);
+    props.showAlert("Text converted to upper case","success");
  }
  const handleLowButton=()=>{
     // console.log("Upper case button clicked");
     let newText=text.toLowerCase();
     setText(newText);
+    props.showAlert("Text converted to lower case","success");
+ }
+
+ const handleClearText=()=>{
+   let newText=text.toLowerCase();
+    setText("");
+    props.showAlert("Text cleared","success");
+
  }
 
 
@@ -19,22 +29,23 @@ export default function TextForm(props) {
  const handleOnChange=(event)=>{
     // console.log("text changed");
     setText(event.target.value);
-
+    
  }
   return (
     <>
-    <h1>{props.defaultContent}</h1>
+    <h1 style={{color:props.mode==='dark'?'white':'black'}}>{props.defaultContent}</h1>
     <div className="form-floating">
         
-        <textarea className="form-control" placeholder="Leave a comment here" style={{height: 300+'px'}} id="floatingTextarea" value={text} onChange={handleOnChange}></textarea>
+        <textarea className="form-control" placeholder="Leave a comment here" style={{height: 300+'px',color:props.mode==='dark'?'white':'black',backgroundColor:props.mode==='dark'?'grey':'white'}} id="floatingTextarea" value={text} onChange={handleOnChange}></textarea>
         <label htmlFor="floatingTextarea">Comments</label>
-        <button className="btn btn-success my-3 mx-1" onClick={handleUpButton}>Change to UpperCase</button>
-        <button className="btn btn-success my-3 mx-1" onClick={handleLowButton}>Change to LowerCase</button>
+        <button className="btn btn-success my-3 mx-1" onClick={handleUpButton}  style={{color:props.mode==='dark'?'white':'black'}}>Change to UpperCase</button>
+        <button className="btn btn-success my-3 mx-1" onClick={handleLowButton} style={{color:props.mode==='dark'?'white':'black'}}>Change to LowerCase</button>
+        <button className="btn btn-success my-3 mx-1" onClick={handleClearText} style={{color:props.mode==='dark'?'white':'black'}}>Clear Text</button>
 
     </div>
-    <h2>Text Summary</h2>
-    <p>{text.split(" ").length} words and {text.length} characters</p>
-    <p>{parseFloat(0.008*text.split(" ").length,2).toFixed(2)} Minutes read</p>
+    <h2 style={{color:props.mode==='dark'?'white':'black'}}>Text Summary</h2>
+    <p style={{color:props.mode==='dark'?'white':'black'}}>{text.split(" ").length} words and {text.length} characters</p>
+    <p style={{color:props.mode==='dark'?'white':'black'}}>{parseFloat(0.008*text.split(" ").length,2).toFixed(2)} Minutes read</p>
     </>
   )
 
